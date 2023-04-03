95ad4113653126d2eba2e5a53eb12bcc7b6e3b02
Dev <127723592+DevCoderByjus@users.noreply.github.com>
2023-04-03 12:02:45 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Get the AI element by class name
+const aiElement = document.querySelector('.ai');
+
+// Add a click event listener to the AI element
+aiElement.addEventListener('click', function() {
+  // Add the 'clicked' class to the AI element to apply the click animation
+  aiElement.classList.add('clicked');
+  
+  // Remove the 'clicked' class after the animation is finished
+  setTimeout(function() {
+    aiElement.classList.remove('clicked');
+  }, 1000); // Set the timeout to match the duration of the click animation
+});
